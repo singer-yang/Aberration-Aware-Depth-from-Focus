9b8c041979b5e449b659a286a0e1612ac04c66e7
1253381145 <36985433+1253381145@users.noreply.github.com>
2024-07-08 02:22:56 +0800

    Add files via upload

--- 0_warm_up.py
+++ 0_warm_up.py
@@ -4,21 +4,23 @@ from deeplens.psfnet import PSFNet
 from torchvision.utils import save_image
 
 if __name__ == "__main__":
-
     # Load lens and PSFNet
     psfnet = PSFNet(filename='./lenses/rf50mm/lens.json', sensor_res=(480, 640), kernel_size=11)
     psfnet.load_net('./ckpt/rf50mm/PSFNet480x640_ks11.pkl')
     psfnet.analysis()
 
     # Read image
-    img = cv.resize(cv.cvtColor(cv.imread('./dataset/Middlebury2014/Adirondack-perfect/im0.png'), cv.COLOR_BGR2RGB), (640, 480))
+    img = cv.resize(cv.cvtColor(cv.imread('./dataset/Middlebury2014/Adirondack-perfect/im0.png'), cv.COLOR_BGR2RGB),
+                    (640, 480))
     img = torch.tensor(img).permute(2, 0, 1).unsqueeze(0).float() / 255
-    depth = cv.resize(cv.imread(f'./dataset/Middlebury2014/Adirondack-perfect/depth.png', -1) / 1000., (640, 480))
+    depth = cv.resize(cv.imread(f'./dataset/Middlebury2014/Adirondack-perfect/disp0.pfm', -1) / 1000., (640, 480))
     depth = torch.tensor(depth).unsqueeze(0).unsqueeze(0).float()
-    
+
     # Render an image
-    depth = - depth * 1e3 # unit [mm]
-    focus_dist = torch.tensor([-2400.]) # unit [mm]
+    depth = - depth * 1e5  # unit [mm]
+    focus_dist = torch.tensor([-5000.])  # unit [mm]
+
+    # Render an image
     defocused_img = psfnet.render(img.to(psfnet.device), depth.to(psfnet.device), focus_dist.to(psfnet.device))
-    save_image(defocused_img, './aberrated_defocused_img.png')
+    save_image(defocused_img, './aberrated_defocused_img_1_1000.png')
     save_image(img, './all_in_focus_img.png')
